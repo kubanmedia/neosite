473a635f324fe4f63bcf75c980c779041c9e6c97
kubanmedia <kubanmedia@yahoo.com>
2026-01-02 23:44:22 +0400

    Install React dependencies and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html')
-    }
-  },
-  root: '.'
+  }
 })
